368378389e063dfe3f72110ba211d6a26173ff7f
shay-studio <cestshay@gmail.com>
2021-06-15 18:23:28 +0200

    Update the current time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,32 @@
+function formatDate(timestemp) {
+  //Calculate the date
+
+  let date = new Date(timestemp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  //Convert numbers to days
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -10,6 +39,11 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let precipitationsElement = document.querySelector("#precipitations");
 
+  //Date
+
+  let currentTime = document.querySelector("#date");
+  currentTime.innerHTML = formatDate(response.data.dt * 1000);
+
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   precipitationsElement.innerHTML = response.data;
@@ -18,6 +52,7 @@ function displayTemperature(response) {
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
 }
+
 let city = "Barcelona";
 let apiKey = "3b640c0f817d39516bf582b1b906ce4f";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
